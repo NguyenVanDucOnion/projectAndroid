12fdda05055fc17189e6cc07c0a1e25d560d2323
QuyetTN <quyetnguyen3302002@gmail.com>
2024-01-19 09:29:37 +0700

    fix

--- app/src/main/java/com/example/androidprojectbtl/View/Admin/AdminHomeActivity.java
+++ app/src/main/java/com/example/androidprojectbtl/View/Admin/AdminHomeActivity.java
@@ -6,18 +6,26 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.androidprojectbtl.R;
+import com.example.androidprojectbtl.View.Account.SignInActivity;
 import com.example.androidprojectbtl.View.Admin.Product.ListProductActivity;
+import com.example.androidprojectbtl.View.HomeActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminHomeActivity extends AppCompatActivity {
-    CardView btnProductManage;
+    CardView btnProductManage, cSignOut;
+    private FirebaseAuth firebaseAuth;
 
+    ImageView ivhoadon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
         btnProductManage = findViewById(R.id.btnProductManage);
+        cSignOut = findViewById(R.id.cSignOut);
+        ivhoadon = findViewById(R.id.ivhoadon);
         initEvents();
     }
 
@@ -27,6 +35,20 @@ public class AdminHomeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(AdminHomeActivity.this, ListProductActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        cSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();startActivity(new Intent(AdminHomeActivity.this, SignInAdminActivity.class));finish();
+            }
+        });
+
+        ivhoadon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminHomeActivity.this, BillADMiNActivity.class));
             }
         });
     }
